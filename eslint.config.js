aed944ce62582f567a5e7e3b7c826374bbe0e47e
umarkhan905 <itxumarkhan905@gmail.com>
2025-07-23 13:12:14 +0500

    Setup Backend with node + express along with prettier, eslint, lint-staged and husky.

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended", eslintConfigPrettier],
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        languageOptions: { globals: globals.node },
+    },
+    tseslint.configs.recommended,
+]);
